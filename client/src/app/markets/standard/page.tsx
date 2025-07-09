0d75e92276584c173a9f5760a97b85cb7ad1b664
SurajTrs <112409164+SurajTrs@users.noreply.github.com>
2025-07-10 02:08:49 +0530

    Update project files

---
+++ client/src/app/markets/standard/page.tsx
@@ -0,0 +1,20 @@
+
+'use client';
+
+
+import StandardBanner from '@/components/Account/type/Standard/StandardBanner';
+
+import React from 'react';
+
+
+
+
+const StandardPage = () => (
+   <main>
+  <StandardBanner/>
+ 
+  </main>
+  
+);
+
+export default StandardPage;
